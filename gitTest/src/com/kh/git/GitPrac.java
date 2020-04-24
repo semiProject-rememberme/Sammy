94eed013e4478351e65e281c33ac7d7957852175
user1 <user1@DESKTOP-2LFI64H>
2020-04-24 12:32:51 +0900

    난 조원이야

--- gitTest/src/com/kh/git/GitPrac.java
+++ gitTest/src/com/kh/git/GitPrac.java
@@ -5,6 +5,7 @@ public class GitPrac {
 	public static void main(String[] args) {
 		System.out.println("hello");
 		System.out.println("안녕?");
+		System.out.println("조장");
 		System.out.println("조원");
 	}
 
